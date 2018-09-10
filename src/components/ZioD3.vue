881864a162318cdbb93da4103465aee0e84ad7f1
Sreng Shuan <srengtrecom@yahoo.com>
2018-09-10 22:28:16 +0700

    zio_d3 component added.

--- src/components/ZioD3.vue
+++ src/components/ZioD3.vue
@@ -1,5 +1,6 @@
 <template>
-  <svg width="200" height="200"></svg>
+  <svg class="svg-container" >
+  </svg>
 </template>
 
 <script>
@@ -7,8 +8,43 @@
 
     export default {
         name: 'zio-d3',
+        data() {
+            return {
+
+            };
+        },
         props: {
+            cx: {
+                type: Number,
+                default() {
+                    return {};
+                },
+            },
+            cy: {
+                type: Number,
+                default() {
+                    return {};
+                },
+            },
+            r: {
+                type: Number,
+                default() {
+                    return {};
+                },
+            },
             zioText: {
+                type: String,
+                default() {
+                    return {};
+                },
+            },
+            colors: {
+                type: Object,
+                default() {
+                    return {};
+                },
+            },
+            defaultColor: {
                 type: String,
                 default() {
                     return {};
@@ -17,25 +53,42 @@
         },
         methods: {
             drawCircle() {
+                d3.select(this.$el).selectAll('circle, text').remove();
                 d3.select(this.$el)
                     .append('circle')
-                    .attr('cx', '100')
-                    .attr('cy', '100')
-                    .attr('r', '100')
+                    .attr('cx', this.cx)
+                    .attr('cy', this.cy)
+                    .attr('r', this.r)
                     .style('stroke', '#000')
                     .style('stroke-width', '1px')
-                    .style('fill', '#d0e1f3');
+                    .style('fill', this.defaultColor);
                 d3.select(this.$el)
                     .append('text')
-                    .attr('x', '50%')
-                    .attr('y', '50%')
+                    .attr('class', 'zio__text')
+                    .attr('x', this.cx)
+                    .attr('y', this.cy)
                     .attr('text-anchor', 'middle')
                     .text(this.zioText);
                 console.log(this.zioText);
             },
+            getWindowWidth() {
+                this.cx = document.documentElement.clientWidth / 2;
+            },
+        },
+        computed: {
+
         },
         mounted() {
             this.drawCircle();
+            this.$nextTick(() => {
+                window.addEventListener('resize', () => {
+                    this.drawCircle();
+                    this.getWindowWidth();
+                });
+            });
+        },
+        beforeDestroy() {
+            window.removeEventListener('resize', () => this.getWindowWidth);
         },
         watch: {
             zioText: 'drawCircle',
@@ -44,8 +97,20 @@
 </script>
 
 <style lang="scss" scoped>
-  svg {
-    padding: 1em;
+  .svg-container {
+    display: inline-block;
+    position: relative;
+    width: 100%;
+    padding-top: 50px;
+    padding-bottom: 10%;
+    vertical-align: middle;
+    overflow: hidden;
+    /*margin: auto;*/
+  }
+  .zio__text {
+    width: 10%;
+    max-width: 10%;
+    display: block;
   }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,30 +3,73 @@
     <h1>Zio Front-End Developer Sample Task</h1>
 
     <div class="zio">
-      <!--<label for="zio__input">zio text: </label>-->
-      <input v-model="zioText" id="zio__input" class="zio__input"
-             placeholder="Please type here">
-    </div>
-    <div class="zio__d3">
-      <zio-d3 :zio-text="zioText"></zio-d3>
+      <label for="zio__input">zio radius: </label>
+      <input
+              type="number"
+              :min="10"
+              :max="300"
+              :step="10"
+              v-model="zioRadius"
+              id="zio__radius"
+              class="zio__radius">
+      <!--<chrome-picker v-model="colors" :colors="colors.hex"></chrome-picker>-->
+      <!--Selected Color: {{colors.hex}}-->
+      <br/>
+      <label for="zio__radius">zio text: </label>
+      <input
+              v-model="zioText"
+              id="zio__input"
+              class="zio__input"
+              placeholder="Please type here">
     </div>
 
+    <div class="zio__d3">
+      <zio-d3
+              :zio-text="zioText"
+              :cx="cx1"
+              :cy="r1"
+              :r="r1"
+              :defaultColor="defaultColor">
+      </zio-d3>
+      <zio-d3
+              :zio-text="zioText"
+              :cx="cx2"
+              :cy="Number(zioRadius)"
+              :r="Number(zioRadius)"
+              :defaultColor="defaultColor">
+      </zio-d3>
+    </div>
   </div>
 </template>
 
 <script>
     // @ is an alias to /src
+    // import {Chrome} from 'vue-color';
     import ZioD3 from '../components/ZioD3.vue';
 
     export default {
         name: 'home',
         components: {
             ZioD3,
+            // 'chrome-picker': Chrome,
         },
         data() {
             return {
                 zioText: 'Sample Text',
+                colors: {
+                    hex: '#d0e1f3',
+                },
+                defaultColor: '#d0e1f3',
+                zioRadius: 100,
+                cx1: window.innerWidth / 2,
+                r1: 70,
+                cx2: window.innerWidth / 2,
             };
+        },
+        methods: {
+            getColor() {
+                return this.colors.hex;
+            },
         },
     };
 </script>
@@ -38,10 +81,21 @@
   }
   .zio {
     padding-top: 90px;
+    text-align: center;
     &__input {
       font-size: 18px;
       font-weight: 500;
       text-align: center;
+      margin-top: 30px;
+    }
+    &__radius {
+      max-width: 70px;
+      font-size: 18px;
+      font-weight: 500;
+      text-align: center;
+    }
+    .vc-chrome {
+      margin: 30px auto;
     }
   }
 </style>
